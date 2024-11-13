c20e64f53b423d2b04316de6c83dcfb1255493fb
Guvener Gokce <guvener@guvenergokce.com>
2024-11-13 10:25:51 +0100

    package.json

--- examples/basic-alone/vite.config.mjs
+++ examples/basic-alone/vite.config.mjs
@@ -1,35 +1,28 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from'vite-plugin-mkcert'
-import uhuu from 'vite-plugin-uhuu'
-import fs from 'fs';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
+import uhuu from 'vite-plugin-uhuu';
 import path from 'path';
 
-export default ({mode, command}) => {
-    // Load app-level env vars to node-level env vars.
-    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+export default ({ mode }) => {
+  // Load environment variables
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const event = process.env.npm_lifecycle_event;
 
-    let config = {
-      base: '',
-      resolve:{
-        alias:{
-          '@' : path.resolve(__dirname, './src') // alias to src directory
-        }
-      },
-      plugins: [react()]
-    };
+  const config = {
+    base: '',
+    resolve: {
+      alias: { '@': path.resolve(__dirname, './src') },
+    },
+    plugins: [react()],
+  };
 
-    // uhuu plugin
-    if(process.env?.npm_lifecycle_event == "uhuu")
-      config.plugins.push(uhuu());
-    
-    // custom certificate
-    if(process.env?.npm_lifecycle_event == "dev:secure" || process.env.LERNA_PACKAGE_NAME)
-      config.plugins.push(mkcert({savePath: './../../.cert'}));    
-    
-    // move to public folder if it's built with lerna
-    if(command == "build" && process.env.LERNA_PACKAGE_NAME)
-      config.build = { outDir : "./../../public/" + process.env.npm_package_name, emptyOutDir: true }
+  // Add uhuu plugin for uhuu and build events
+  if (event === 'uhuu' || event === 'build') config.plugins.push(uhuu());
+  // Install SSL certificate for secure local development
+  if (event === 'dev:secure' || process.env.LERNA_PACKAGE_NAME) config.plugins.push(mkcert({ savePath: './../../.cert' }));
+  // Output to public folder when building all with lerna
+  if (event === 'build:all') config.build = { outDir: `./../../public/${process.env.npm_package_name}`, emptyOutDir: true };
 
-    return defineConfig(config);
-}
+  return defineConfig(config);
+};
